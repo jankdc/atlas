27f89dbe6797ff364f3d73bf18cb081fa981b91c
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2015-04-30 04:04:46 +0100

    Main is now command-line friendly!

--- build.sbt
+++ build.sbt
@@ -1,6 +1,11 @@
+import com.github.retronym.SbtOneJar._
+
+oneJarSettings
+
 lazy val root = (project in file(".")).
   settings(
     name := "atlas",
     version := "0.1.0",
     scalaVersion := "2.11.5"
   )
+
--- src/main/scala/atlas/Main.scala
+++ src/main/scala/atlas/Main.scala
@@ -10,60 +10,97 @@ import scala.io.Source
 import scala.sys.process._
 import java.io.{File, FileWriter, BufferedWriter}
 
-object Main extends App {
-  try {
-    val path = "/atom.atlas"
-    val source = Source.fromURL(getClass.getResource(path)).mkString
-    println(s"Location: $path")
+object Main {
+  // Command Line Outputs
+  lazy val tooManyArguments = "atlas: error: too many arguments"
+  lazy val usage = "Usage: atlas [src-file]"
 
-    val tokens = mkTokens(source)
-    println(s"Tokens:")
-    println(tokens.map(toString(_)).mkString("\n"))
+  // Built-in Compiler Features.
+  lazy val buildInTps = Set("Unit", "Int", "Boolean")
+  lazy val builtInFns =
+   Map(buildFnSym("println", Seq("Int")) -> types.Var("Unit"),
+       buildFnSym("println", Seq("Boolean")) -> types.Var("Unit"))
 
-    val astree = mkASTree(tokens)
-    val petree = partEval(astree)
-    println("ASTree:")
-    println(petree)
+  def main(args: Array[String]): Unit = {
 
-    val prelude = Set("Unit", "Int", "Boolean")
-    val builtin = Map(
-      Symbol("", "println", "(Int)")(LinePos(1, 1), true, true, 1)
-        -> types.Var("Unit"),
-      Symbol("", "println", "(Boolean)")(LinePos(1, 1), true, true, 1)
-        -> types.Var("Unit"))
-    val context = Context(prelude, builtin)
-    val nodeMap = collectTypes(context, petree)
+    // Process Command Line Arguments.
+    val srcPath = args.toSeq match {
+      case Seq(src) =>
+        src
+      case Seq() =>
+        println(usage)
+        return ()
+      case other =>
+        println(tooManyArguments)
+        return ()
+    }
 
-    val genCode = genLLVM(petree)(nodeMap)
-    val genString = genCode.mkString("\n")
-    println("LLVM IR:")
-    println(genString)
+    val debugMode = true
 
-    val outputFile = new File("bin/main.ll")
-    val output = new BufferedWriter(new FileWriter(outputFile))
-    output.write(genString)
-    output.close()
+    try {
+      val filePath = new File(srcPath)
+      val stream = Source.fromFile(filePath)
+      val source = stream.mkString
+      val tokens = mkTokens(source)
 
-    println("LLVM Output: ")
-    ("llc -filetype=obj bin/main.ll -o bin/main.o" #&&
-     buildLinkerCommand() #&&
-     "./bin/main"
-    ).!
+      if (debugMode) {
+        println("Source:")
+        println(source)
+        println("Tokens:")
+        println(tokens.map(toString(_)).mkString("\n"))
+      }
+
+      val astree = mkASTree(tokens)
+      val petree = partEval(astree)
+
+      if (debugMode) {
+        println("ASTree:")
+        println(petree)
+      }
+
+      val context = Context(buildInTps, builtInFns)
+      val nodeMap = collectTypes(context, petree)
+      val genCode = genLLVM(petree)(nodeMap)
+      val genString = genCode.mkString("\n")
+
+      val prefix = filePath.getName.split('.').head
+      val fullnm = prefix + ".ll"
+      val output = new BufferedWriter(new FileWriter(new File(fullnm)))
+      output.write(genString)
+      output.close()
+
+      if (debugMode) {
+        println("LLVM Input Name:")
+        println(prefix)
+        (buildLLC(fullnm, prefix) #&& buildLinker(prefix + ".o", prefix)).!
+      }
+      else {
+        (buildLLC(fullnm, prefix) #&& buildLinker(prefix + ".o", prefix))
+      }
+    }
+    catch {
+      case err: ParserError =>
+        println(s"[error]${err.getMessage}")
+      case err: TypeError =>
+        println(s"[error]${err.getMessage}")
+      case err: CodeGenError =>
+        println(s"[error]${err.getMessage}")
+    }
   }
-  catch {
-    case err: ParserError =>
-      println(s"[error]${err.getMessage}")
-    case err: TypeError =>
-      println(s"[error]${err.getMessage}")
-    case err: CodeGenError =>
-      println(s"[error]${err.getMessage}")
+
+  private def buildFnSym(name: String, params: Seq[String]): Symbol = {
+    val pos = LinePos(1, 1)
+    Symbol("", name, s"""(${params.mkString(", ")})""")(pos, true, true, 1)
   }
 
-  private def buildLinkerCommand(): String = {
+  private def buildLLC(src: String, dst: String): String =
+    s"llc -filetype=obj $src -o $dst.o"
+
+  private def buildLinker(src: String, dst: String): String = {
     val arch = System.getProperty("os.arch")
     val osVer = System.getProperty("os.version")
     val osxCmdOpt = s"macosx_version_min $osVer"
-    s"ld -arch $arch -$osxCmdOpt -o bin/main bin/main.o -lSystem"
+    s"ld -arch $arch -$osxCmdOpt -o $dst $src -lSystem"
   }
 
   private def toString(t: Token): String = t match {
